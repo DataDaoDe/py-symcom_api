7c148820078de310ba3e07fb4391e949454c49a3
John Faucett <jwaterfaucett@gmail.com>
2021-11-26 18:48:51 +0100

    Add final summary step

--- symcom/algebra/prime_factors.py
+++ symcom/algebra/prime_factors.py
@@ -1,13 +1,18 @@
-from typing import List, Tuple
+from typing import Dict, List, Tuple, Any, NamedTuple
 from functools import reduce
 from sympy.ntheory import factorint
+
+
+class RenderedStepResult(NamedTuple):
+    step_words: str
+    step_latex: str
 
 def get_prime_factors(n: int) -> List[Tuple[int, int]]:
     result = factorint(n)
     factors = sorted(zip(result.keys(), result.values()), key=lambda t: t[0])
     return factors
 
-def get_steps(factors: List[Tuple[int, int]]) -> List[str]:
+def get_steps(factors: List[Tuple[int, int]]) -> List[Dict[str, Any]]:
     steps = []
     total = reduce(lambda a, b: a * b, [x[0]**x[1] for x in factors])
 
@@ -18,3 +23,21 @@ def get_steps(factors: List[Tuple[int, int]]) -> List[str]:
             total = total // base
 
     return steps
+
+def render_steps(factors: List[Tuple[int, int]], steps: List[Dict[str, Any]]) -> List[RenderedStepResult]:
+    rendered_results = []
+
+    for step in steps:
+        step_words = f"{step['total']} divides by {step['factor']} resulting in {step['division_result']}"
+        step_latex = f"{step['total']} = {step['factor']}\\cdot {step['division_result']}"
+        step_result = RenderedStepResult(step_words=step_words, step_latex=step_latex)
+
+        rendered_results.append(step_result)
+
+    # add summary step
+    just_factors = ', '.join( list(map(lambda x: str(x[0]), factors)) )
+    just_factors += ' are all the prime numbers, therefore no further factorization is possible.'
+
+    rendered_results.append(RenderedStepResult(step_words=just_factors, step_latex=None))
+
+    return rendered_results
--- tests/algebra/prime_factors_test.py
+++ tests/algebra/prime_factors_test.py
@@ -1,4 +1,5 @@
 from symcom.algebra import prime_factors as pf
+from symcom.latex import from_latex
 from unittest import TestCase
 
 def test_get_prime_factors():
@@ -23,3 +24,21 @@ def test_get_steps():
     assert steps[2] == {'total' : 45, 'factor' : 3, 'division_result' : 15}
     assert steps[3] == {'total' : 15, 'factor' : 3, 'division_result' : 5}
     assert steps[4] == {'total' : 5, 'factor' : 5, 'division_result' : 1}
+
+
+def test_render_steps():
+    factors = pf.get_prime_factors(45)
+    steps = pf.get_steps(factors)
+    r = pf.render_steps(factors, steps)
+
+    assert r[0].step_words == "45 divides by 3 resulting in 15"
+    assert from_latex(r[0].step_latex), "evaluates latex expression w/out error"
+
+    assert r[1].step_words == "15 divides by 3 resulting in 5"
+    assert from_latex(r[1].step_latex), "evaluates latex expression w/out error"
+
+    assert r[2].step_words == "5 divides by 5 resulting in 1"
+    assert from_latex(r[2].step_latex), "evaluates latex expression w/out error"
+
+    assert r[3].step_words == "3, 5 are all the prime numbers, therefore no further factorization is possible."
+    assert r[3].step_latex == None, "There is no expression data for the final summary step."
